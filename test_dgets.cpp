9bd9f84f01b4c896984b64ecbc4c0c1de1ad222d
Eyal Ben-David <bdeyal@gmail.com>
2025-09-01 03:19:23 +0300

    Test C FILE* read.
    Read both with dstr_fgets() and iostream '>>' operator in std::string
    Read both with dstr_fgetline and compre to std::getline(in, str)
    All reads compared and must be equal

---
+++ test_dgets.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "dstr.h"
+
+using namespace std;
+
+// Compare to 'dstr_fgets' to 'cin >> str' must be equal
+//
+void test_dgets(const char* fname)
+{
+	FILE* fp = fopen(fname, "r");
+	if (!fp) {
+		fprintf(stderr, "couldn't open %s: %s\n", fname, strerror(errno));
+		exit(1);
+	}
+
+	ifstream in(fname);
+	if (!in) {
+		cerr << "couldn't open "<< fname << ": " << strerror(errno) << endl;
+		exit(1);
+	}
+
+	DSTR d1 = dstrnew_reserve(500);
+	std::string s1;
+
+	for (;;) {
+		int r1 = dstr_fgets(d1, fp);
+		in >> s1;
+
+		if (r1 == EOF) {
+			assert(in.eof());
+			break;
+		}
+
+		assert(s1 == dstrdata(d1));
+	}
+
+	cout << "Good! all lines were equal" << endl;
+
+	dstrfree(d1);
+	fclose(fp);
+}
+//-------------------------------------------------
+
+
+// Compare to 'dstr_getline' to 'std::getline(in, str)' must be equal
+//
+void test_getline(const char* fname)
+{
+	FILE* fp = fopen(fname, "r");
+	if (!fp) {
+		fprintf(stderr, "couldn't open %s: %s\n", fname, strerror(errno));
+		exit(1);
+	}
+
+	ifstream in(fname);
+	if (!in) {
+		cerr << "couldn't open "<< fname << ": " << strerror(errno) << endl;
+		exit(1);
+	}
+
+	DSTR d1 = dstrnew_reserve(500);
+	std::string s1;
+
+	for (;;) {
+		int r1 = dstr_fgetline(d1, fp);
+		std::getline(in, s1);
+
+		if (r1 == EOF) {
+			assert(in.eof());
+			break;
+		}
+
+		assert(s1 == dstrdata(d1));
+	}
+
+	cout << "Good! all lines were equal" << endl;
+
+	dstrfree(d1);
+	fclose(fp);
+}
+//-------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+	const char* fname =
+		(argc > 1) ?
+		argv[1] :
+		__FILE__;
+
+	test_dgets(fname);
+	test_getline(fname);
+}
